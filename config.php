8de1f019d00c2f1ebb6a287ed63076eaa7946eb5
Sarah Sameh Wahba <117436397+SarahSameh19@users.noreply.github.com>
2023-12-06 15:05:10 +0200

    database connection

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$servername="localhost";
+$username = "root";
+$password="";
+$DB="awesome_chocolate";
+
+//create connection
+$conn= mysqli_connect($servername, $username,$password,$DB);
+
+//check connection
+if(!$conn){
+    die("Connection failed:".mysqli_connect_error());
+}
+echo "Connected successfully";
+
+
+?>
